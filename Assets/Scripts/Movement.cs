91321b52bd68cb02882bdf5218f4cf84ccbaf677
João Pedro Rosa <joao.rosa5@outlook.com>
2026-08-13 14:02:18 -0300

    Add Input Action Binding

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+
+    void OnEnable()
+    {
+        thrust.Enable();
+    }
+
+    void Update()
+    {
+        if (thrust.IsPressed())
+        {
+            Debug.Log("Deu certo!");
+        }
+    }
+}
