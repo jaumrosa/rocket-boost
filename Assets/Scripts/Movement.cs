673930c9241ab928da2249d008effdb197a23cbf
João Pedro Rosa <joao.rosa5@outlook.com>
2026-08-13 14:21:30 -0300

    Add Relative Force when pressed

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,17 +3,26 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 1000f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnEnable()
     {
         thrust.Enable();
     }
 
-    void Update()
+
+    void FixedUpdate()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Deu certo!");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 }
