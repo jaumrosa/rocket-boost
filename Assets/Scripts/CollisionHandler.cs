c90e304d10d222c9f72f10bca3db13cff458b674
João Pedro Rosa <joao.rosa5@outlook.com>
2026-08-19 13:13:43 -0300

    Add Respawn

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -16,8 +17,15 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Fuel");
                 break;
             default:
-                Debug.Log("Default");
+                ReloadLevel();
                 break;
         }
+
+        void ReloadLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene);
+        }
+
     }
 }
